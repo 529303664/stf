104de2d531de2cf16b9e196fa99fc30d1dabf3e4
噜噜 <529303664@qq.com>
2019-10-25 10:26:08 +0800

    feat: 添加provider命令

---
+++ bin/provider.sh
@@ -0,0 +1,1 @@
+stf provider --name TcloundChengdu --min-port 7400 --max-port 7700 --connect-sub tcp://132.232.78.75:7114 --connect-push tcp://132.232.78.75:7116 --group-timeout 2000 --public-ip 132.232.78.75 --storage-url http://132.232.78.75:7100/ --adb-host 132.232.78.75 --adb-port 5037 --vnc-initial-size 600x800 allow-remote
